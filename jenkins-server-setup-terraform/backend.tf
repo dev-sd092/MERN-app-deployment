cc3e0074c51770fa8e231791d8d2bea9cd7b510c
dev-sd092 <sanketdesai092@gmail.com>
2025-10-24 06:48:55 +0000

    first commit

---
+++ jenkins-server-setup-terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "sanket-desai-mern-app-bucket"
+    region         = "ap-south-1"
+    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "mern-app-table"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
